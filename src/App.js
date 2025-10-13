76e29e13908afde31cfdad111041f1eed197fba7
gyojincompany <luckyred@gmail.com>
2025-10-13 12:52:43 +0900

    회원 가입 페이지 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './component/Navbar';
+import Home from "./pages/Home";
+import Board from "./pages/Board";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import BoardWrite from "./pages/BoardWrite";
+import BoardDetail from "./pages/BoardDetail";
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className='App'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/board' element={<Board />} />
+        <Route path='/board/write' element={<BoardWrite />} />
+        <Route path='/board/:id' element={<BoardDetail />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import "./Home.css";
+
+function Home() {
+    return (
+        <div className="container">
+            <h1>회사 소개</h1>
+            <p>안녕하세요. Gyojin 컴퍼니 홈페이지에 오신 것을 환영합니다!</p>
+        </div>
+    );
+}
+
+export default Home;
